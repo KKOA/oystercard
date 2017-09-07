d2e6cda3e12048417b6ebeeabb5d3fc7aadd2b45
keith(mac) <ka33145@googlemail.com>
2017-09-07 15:25:44 +0100

    Created Journey class

---
+++ lib/journey.rb
@@ -0,0 +1,21 @@
+class Journey
+
+  MIN_FEE = 1
+  MAX_FEE = 6
+
+
+
+
+  # in_journey
+
+
+  # def initialize(entry,exit)
+
+  # end
+
+  def new_journey(entry,exit)
+    {:entry_station => entry ,:exit_station => exit }
+  end
+
+
+end
